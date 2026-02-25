7342938cc5cac4d39c2d9f7b03ae43e6c9a451a3
Yvan <yvan.savergne@gmail.com>
2026-02-25 18:02:25 +0100

    suppression de styles inutiles

--- frontend/app/styles/general.ts
+++ frontend/app/styles/general.ts
@@ -1,3 +1,5 @@
+const titres = 'font-semibold tracking-tighter';
+
 export const GENERAL_STYLES = {
   /* BOUTONS */
   GREENBUTTON: 'bg-vert px-2 py-2 text-black font-semibold cursor-pointer rounded-full',
@@ -5,4 +7,11 @@ export const GENERAL_STYLES = {
   /* ALIGNEMENTS */
   LIGNE_CENTREE: 'flex items-center',
   FLEX_COL: 'flex flex-col',
+
+  /* TRANSITIONS */
+  ZOOM_SURVOL: 'transition-transform hover:scale-105',
+
+  /* TEXTES */
+  TITRES_BLANC: `${titres} text-white`,
+  TITRES_NOIR: `${titres} text-black`,
 }
