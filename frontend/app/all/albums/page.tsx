4b1acf617d48bea54a96e558768bf8122ede98f3
Yvan <yvan.savergne@gmail.com>
2026-02-27 00:31:23 +0100

    test build Suspense

--- frontend/app/all/albums/page.tsx
+++ frontend/app/all/albums/page.tsx
@@ -3,6 +3,8 @@
 import RankingView from "../../components/rankings/RankingView";
 import { useRankingLogic } from "@/app/hooks/useRankingLogic";
 import { useApiAllDatas } from "@/app/hooks/useApiAllDatas";
+import { Suspense } from "react";
+import SkeletonRanking from "@/app/components/rankings/SkeletonRanking";
 
 export default function AlbumsPage() {
   const { getAlbums, getAlbumsMetadata } = useApiAllDatas();
@@ -10,11 +12,14 @@ export default function AlbumsPage() {
     useRankingLogic(getAlbums, getAlbumsMetadata, 'album');
 
   return (
-    <RankingView 
-      title="Tous les" type="album" items={items}
-      sortConfig={currentSort} onSort={handleSort} loading={status.loading}
-      hasMore={status.hasMore} loadMore={() => fetchData(status.offset + 50, false)} 
-      filterConfig={filterConfig}
-    />
+    <Suspense fallback={<SkeletonRanking/>}>
+      <RankingView 
+        title="Tous les" type="album" items={items}
+        sortConfig={currentSort} onSort={handleSort} loading={status.loading}
+        hasMore={status.hasMore} loadMore={() => fetchData(status.offset + 50, false)} 
+        filterConfig={filterConfig}
+      />
+    </Suspense>
+    
   );
 }
