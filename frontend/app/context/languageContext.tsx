a241991ad62a8dbef7a2bef570bc5a14f04160ca
Yvan <yvan.savergne@gmail.com>
2026-03-21 20:47:59 +0100

    site en francais pour l'instant

--- frontend/app/context/languageContext.tsx
+++ frontend/app/context/languageContext.tsx
@@ -12,7 +12,7 @@ interface LanguageContextType {
 const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
 
 export function LanguageProvider({ children }: { children: React.ReactNode }) {
-  const [language, setLanguage] = useState<LanguageType>("en");
+  const [language, setLanguage] = useState<LanguageType>("fr");
 
   useEffect(() => {
     const saved = localStorage.getItem('language') as LanguageType;
